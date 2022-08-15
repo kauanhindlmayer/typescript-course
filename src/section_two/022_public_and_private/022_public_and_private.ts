20e9de257855da6f46b6c4f58458b9e22916cac1
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-08-15 17:05:57 -0300

    Exercises

---
+++ src/section_two/022_public_and_private/022_public_and_private.ts
@@ -0,0 +1,33 @@
+export class Company {
+  readonly name: string; // public isn't necessary
+  private readonly employees: Employe[] = [];
+  protected readonly ein: string;
+
+  constructor(name: string, ein: string) {
+    this.name = name;
+    this.ein = ein;
+  }
+
+  addEmploye(employe: Employe): void {
+    this.employees.push(employe);
+  }
+
+  showEmployees(): void {
+    for (const employe of this.employees) {
+      console.log(employe);
+    }
+  }
+}
+
+export class Employe {
+  constructor(public readonly name: string, public readonly lastname: string) {}
+}
+
+const myCompany = new Company('Company', '00.000.000/0000-00');
+const employe1 = new Employe('Kauan', 'Hindlmayer');
+const employe2 = new Employe('Luiz', 'Otávio');
+const employe3 = new Employe('João', 'Vieira');
+myCompany.addEmploye(employe1);
+myCompany.addEmploye(employe2);
+myCompany.addEmploye(employe3);
+console.log(myCompany.name);
