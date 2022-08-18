5ab2b08ea52f467d662b0ef3342d9c61dd2bb58e
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-08-18 16:28:57 -0300

    Exercises

--- frontend/assets/js/bundle.js
+++ frontend/assets/js/bundle.js
@@ -1,6 +1,59 @@
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/section_two/035_exercise_video/035_exercise_video.ts":
+/*!******************************************************************!*\
+  !*** ./src/section_two/035_exercise_video/035_exercise_video.ts ***!
+  \******************************************************************/
+/***/ (() => {
+
+
+console.log('Hello, World!');
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
 /******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
 /******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+var exports = __webpack_exports__;
+/*!******************************************************!*\
+  !*** ./src/section_one/019_exercise/019_exercise.ts ***!
+  \******************************************************/
+
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__webpack_require__(/*! ../../section_two/035_exercise_video/035_exercise_video */ "./src/section_two/035_exercise_video/035_exercise_video.ts");
+
+})();
+
 /******/ })()
 ;
+//# sourceMappingURL=bundle.js.map
