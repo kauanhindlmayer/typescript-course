49db2889af17833a28ceb944f47c6f268752b279
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-08-22 09:21:11 -0300

    Exercises

---
+++ src/section3/040_this_polymorphic/040_this_polymorphic.ts
@@ -0,0 +1,57 @@
+export class Calculator {
+  constructor(public number: number) {}
+
+  add(n: number): this {
+    this.number += n;
+    return this;
+  }
+
+  sub(n: number): this {
+    this.number -= n;
+    return this;
+  }
+
+  div(n: number): this {
+    this.number /= n;
+    return this;
+  }
+
+  mul(n: number): this {
+    this.number *= n;
+    return this;
+  }
+}
+
+export class SubCalculator extends Calculator {
+  pow(n: number): this {
+    this.number **= n;
+    return this;
+  }
+}
+
+const calculator = new SubCalculator(10);
+calculator.add(5).mul(2).div(2).sub(5).pow(2);
+console.log(calculator);
+
+// Builder - GoF
+export class RequestBuilder {
+  private method: 'get' | 'post' | null = null;
+  private url: string | null = null;
+
+  setMethod(method: 'get' | 'post'): this {
+    this.method = method;
+    return this;
+  }
+
+  setUrl(url: string): this {
+    this.url = url;
+    return this;
+  }
+
+  send(): void {
+    console.log(`Sending data through ${this.method} to ${this.url}`);
+  }
+}
+
+const request = new RequestBuilder(); // Builder
+request.setUrl('http://www.google.com').setMethod('post').send();
