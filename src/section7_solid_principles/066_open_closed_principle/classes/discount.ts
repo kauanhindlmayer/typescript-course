056d8c10faba0296d3616e156ec790cb0a1c8e2e
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-01 13:58:59 -0300

    Exercises

--- src/section7_solid_principles/066_open_closed_principle/classes/discount.ts
+++ src/section7_solid_principles/066_open_closed_principle/classes/discount.ts
@@ -1,27 +1,17 @@
 export abstract class Discount {
-  abstract calculate(value: number): number;
+  protected discount = 0;
+
+  calculate(price: number): number {
+    return +(price - price * (this.discount / 100)).toFixed(2);
+  }
 }
 
 export class FiftyPercentDiscount extends Discount {
-  private readonly discount = 50;
-
-  calculate(price: number): number {
-    return +(price - price * (this.discount / 100)).toFixed(2);
-  }
+  protected readonly discount = 50;
 }
 
 export class TenPercentDiscount extends Discount {
-  private readonly discount = 10;
-
-  calculate(price: number): number {
-    return +(price - price * (this.discount / 100)).toFixed(2);
-  }
+  protected readonly discount = 10;
 }
 
-export class NoDiscount extends Discount {
-  private readonly discount = 10;
-
-  calculate(price: number): number {
-    return +price.toFixed(2);
-  }
-}
+export class NoDiscount extends Discount {}
