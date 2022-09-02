d9affb17ef5c432f162e450ae7ec552f087fccab
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-02 14:00:17 -0300

    Exercises

---
+++ src/section7_solid_principles/070_advantages_and_disadvantages/index.ts
@@ -0,0 +1,19 @@
+/*
+  Vantagens e Desvantagens dos princípios S.O.L.I.D.
+
+  Vantagens:
+  - Código modular;
+  - Código reutilizável (D.R.Y. - Don't Repeat Yourself);
+  - Código testável;
+  - Baixo acoplamento e alta coesão;
+  - Código expansível;
+  - Separation of concerns (Separação de conceitos);
+  - Fácil manutenção.
+
+  Desvantagens:
+  - Complexidade;
+  - Quantidade de código digitado aumenta drasticamente;
+  - Tempo de desenvolvimento aumenta bastante;
+  - Tenha cuidados com: YAGNI (You aren't gonna need it)
+  e KISS (Keep it simple stupid!).
+*/
