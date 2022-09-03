d18a5b8ce5f771e1802a31e29232929bcf58079b
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-03 11:59:52 -0300

    Exercises

---
+++ src/section8_tests_with_jest/services/persistency.spec.ts
@@ -0,0 +1,10 @@
+describe('Testando alguma coisa', () => {
+  it('descrição do teste (IT)', () => {
+    const number = 1;
+    expect(number).toBe(1);
+  });
+  test('descrição do teste (TEST)', () => {
+    const nome = 'Kauan';
+    expect(nome).toBe('Kauan');
+  });
+});
