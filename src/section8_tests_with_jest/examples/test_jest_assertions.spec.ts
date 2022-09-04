359a0f6051cc172ca9219f9026af810e6fe63d27
Kauan Hindlmayer <kauanhindlmayer07@gmail.com>
2022-09-04 10:50:38 -0300

    Exercises

--- src/section8_tests_with_jest/examples/test_jest_assertions.spec.ts
+++ src/section8_tests_with_jest/examples/test_jest_assertions.spec.ts
@@ -10,9 +10,33 @@ describe('Primitive values', () => {
 
     expect(number).toBeGreaterThan(9);
     expect(number).toBeGreaterThanOrEqual(10);
+  });
+  it('should split tests', () => {
+    const number = 10;
+
     expect(number).toBeLessThanOrEqual(10);
     expect(number).toBeLessThan(11);
 
     expect(number).toBeCloseTo(10.001);
+    expect(number).toBeCloseTo(9.996);
+
+    expect(number).not.toBeNull();
+
+    expect(number).toHaveProperty('toString');
+  });
+});
+
+describe('Objects', () => {
+  it('should test jest assertions with objects', () => {
+    const person = { name: 'Kauan', age: 18 };
+    const anotherPerson = { ...person };
+
+    expect(person).toBe(anotherPerson); // Fail
+    expect(person).toEqual(anotherPerson); // Success
+
+    expect(person).toHaveProperty('age', 18);
+    expect(person).not.toHaveProperty('lastName');
+
+    expect(person.name).toBe('Kauan');
   });
 });
